31f74ce66de708d43f5a8b22337fc943edb16c24
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2015-05-17 03:24:56 +1200

    Tested
    Minor changes to html.
    Adjusted margins/padding.
    Finished and automated jQuery.
    css styles auto update using jQuery.
    Tested for buttons of differing sizes.

--- text-viewer.js
+++ text-viewer.js
@@ -1,73 +1,182 @@
+// All values are in px
+// Single viewer dimensions
+textWidth		= 400
+leftHeight		= 350
+rightHeight		= 450
+// Double viewer dimensions
+fullWidth 		= 550
+fullHeight		= 550
+
+// Adjustments to button layout
+// Gap between buttons
+buttonSpacing	= 5
+// Top buttons moving further away from the center
+viewerButtonXadjust	= 0
+// Bottom button moving further to the left
+bottomButtonXadjust	= 0
+// Buttons moving further upwards
+viewerButtonYadjust	= 0
+bottomButtonYadjust	= 0
+
 $(document).ready(function() {
-
-	$('.viewer-button').on('click', function() {
-		if ($(this).is('#left-button')) {
-			checkStatus('#left-text', '#right-text', 'colourA');
-		} else if ($(this).is('#right-button')) {
-			checkStatus('#right-text', '#left-text', 'colourB');
-		} else {
-
-		};
-	})
+	getValues();
+	setLayout();
+	buttonClick();
 });
 
-function checkStatus(selectedEle, otherEle, bgColour) {
-	if ($(selectedEle).hasClass('hidden')) {
-		closeViewer(selectedEle, otherEle, bgColour);
-		openViewer(selectedEle, otherEle);
-	} else {
-		closeViewer(selectedEle, otherEle);
-	};
+// Start of functions
+// get button dimensions
+function getValues() {
+	viewerButtonWidth	= $('.viewer-button').width();
+	viewerButtonHeight	= $('.viewer-button').height();
+	bottomButtonWidth	= $('.bottom-button').width();
+	bottomButtonHeight	= $('.bottom-button').height();
+	bottomShift = -bottomButtonHeight*0.5;
 }
-
-function openViewer(selectedEle, otherEle) {
-	// Buttons when viewer is open (Left, Right, All, None)
-	// Left and right are distance from the center
-	// All and none are distance from the bottom
-	animateButtons('-225', '175', '-25', '-25');
-	$('.text-container').animate({
-		// Viewer maximums
-		"width": '400',
-		"height": '250'
-	}, function() {
-		showText(selectedEle, otherEle);
+// Dynamic layout
+function setLayout() {
+	$('#text-viewer-container').css({
+		"width":fullWidth-viewerButtonWidth,
+		"height": fullHeight
+	});
+	$('#viewer-button-container').css({
+		"width": fullWidth-viewerButtonWidth,
+		"height": viewerButtonHeight,
+		"margin-bottom": -(viewerButtonHeight/2+viewerButtonYadjust)
+	});
+	$('#left-button').css({
+		"margin-left": -(viewerButtonWidth+buttonSpacing)
+	});
+	$('#right-button').css({
+		"margin-left": 0
+	});
+	$('.bottom-button').css({
+		"margin-left": -bottomButtonWidth/2-(bottomButtonXadjust+buttonSpacing/2),
+		"margin-top": (bottomShift-bottomButtonYadjust)+viewerButtonHeight/2
 	});
 }
-
-function closeViewer(selectedEle, otherEle, bgColour) {
-	// Buttons when viewer is closed (Left, Right, All, None)
-	animateButtons('-55', '0', '-50', '-50');
-	$('.text-container').animate({
-		// Closed viewer
-		"width": '0',
-		"height": '0'
-	}, function() {
-		hideText(selectedEle, otherEle);
-
-		var selectedColour = bgColour
-		if (selectedColour==='colourA') {
-			$('.text-container').removeClass('colourB');
-			$('.text-container').addClass('colourA');
-		} else {
-			$('.text-container').removeClass('colourA');
-			$('.text-container').addClass('colourB');
+// Button click listeners
+function buttonClick() {
+	$('.viewer-button').on('click', function() {
+		if ($(this).is('#left-button')) {
+			checkStatus('#left-text', '#right-text');
+		} else if ($(this).is('#right-button')) {
+			checkStatus('#right-text', '#left-text');
+		};
+	});
+	$('.bottom-button').on('click', function() {
+		if ($(this).is('#all-button')) {
+			if ($('#left-text').children('ul').hasClass('hidden') &&
+			 $('#right-text').children('ul').hasClass('hidden')) {
+				doubleViewer();
+			} else {
+				closeViewer(false, 'double');
+			};
+		} else if ($(this).is('#none-button')) {
+			closeViewer(true)
 		};
 	});
 }
-
-function animateButtons(left, right, all, none) {
-	$('#left-button').animate({"margin-left":left});
-	$('#right-button').animate({"margin-left":right});
-	$('#all-button').animate({"margin-top":all});
-	$('#none-button').animate({"margin-top":none});
+// Checks if button's information was hidden
+function checkStatus(selectedEle, otherEle) {
+	if ($('#all-button').hasClass('hidden')) {
+		closeViewer(false, 'single', selectedEle);
+	} else if ($(selectedEle).children('ul').hasClass('hidden')) {
+		if ($(otherEle).children('ul').hasClass('hidden')) {
+			singleViewer(selectedEle);
+		} else {
+			closeViewer(false, 'single', selectedEle);
+		};
+	} else {
+		closeViewer(true);
+	};
 }
-
-function showText(selectedEle, otherEle) {
-	$(otherEle).addClass('hidden');
-	$(selectedEle).removeClass('hidden');
+// Display's a single tab of information
+function singleViewer(selectedEle) {		
+	if (selectedEle === '#left-text') {
+		var textHeight = leftHeight
+	} else {
+		var textHeight = rightHeight
+	};
+	var leftShift = -(textWidth/2)-(viewerButtonWidth/2)-viewerButtonXadjust
+	var rightShift = (textWidth/2)-(viewerButtonWidth/2)+viewerButtonXadjust
+	animateButtons(leftShift, rightShift, bottomShift);
+	$(selectedEle).animate({
+		"width": textWidth,
+		"height": textHeight
+	});
+	$('#text-container').animate({
+		"width": textWidth,
+		"height": textHeight
+	}, function() {
+		$(selectedEle).children('ul').removeClass('hidden');
+	});
 }
-
-function hideText(selectedEle, otherEle) {
-	$(otherEle).addClass('hidden');
-	$(selectedEle).addClass('hidden');
+// Displays both tabs of information
+function doubleViewer() {
+	var containerWidth = fullWidth-viewerButtonWidth
+	var textBox = containerWidth/2-5
+	var leftShift = -(containerWidth/2)-(viewerButtonWidth/2-(5+viewerButtonXadjust))
+	var rightShift = (containerWidth/2)-(viewerButtonWidth/2+(5+viewerButtonXadjust))
+	$('.text-box').addClass('double-view');
+	$('#left-text').addClass('shift-left');
+	$('#right-text').addClass('shift-right');
+	animateButtons(leftShift, rightShift, bottomShift);
+	$('#left-text').animate({
+		"width": textBox,
+		"height": fullHeight
+	});
+	$('#right-text').animate({
+		"width": textBox,
+		"height": fullHeight
+	});
+	$('#text-container').animate({
+		"width": containerWidth,
+		"height": fullHeight
+	}, function() {
+		$('.text-box').children('ul').removeClass('hidden');
+		bottomButton('#all-button', '#none-button');
+	});
+}
+// Closes all tabs
+function closeViewer(fullClose, viewer, selectedEle) {
+	animateButtons(-(viewerButtonWidth+5), 0, 0);
+	function minimiseEle(thisEle) {
+		$(thisEle).animate({
+			"width": 0,
+			"height": 0
+		});
+	}
+	minimiseEle('#left-text');
+	minimiseEle('#right-text');
+	$('#left-text').children('ul').addClass('hidden');
+	$('#right-text').children('ul').addClass('hidden');
+	$('#text-container').animate({
+		"width": 0,
+		"height": 0
+	}, function () {
+		bottomButton('#none-button', '#all-button');
+		$('.text-box').removeClass('double-view');
+		$('#left-text').removeClass('shift-left');
+		$('#right-text').removeClass('shift-right');
+		if (fullClose === false) {
+			if (viewer === 'single') {
+				singleViewer(selectedEle);
+			} else {
+				doubleViewer();	
+			};
+		};
+	});
+}
+// Moves buttons based on arguments
+function animateButtons(leftShift, rightShift, bottomShift) {
+	$('#left-button').animate({"margin-left":leftShift});
+	$('#right-button').animate({"margin-left":rightShift});
+	$('#all-button').animate({"margin-top": bottomShift});
+	$('#none-button').animate({"margin-top": bottomShift});
+}
+// Selects which bottom button to show
+function bottomButton(hideThis, showThis) {
+	$(hideThis).addClass('hidden');
+	$(showThis).removeClass('hidden');
 }
